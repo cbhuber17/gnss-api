f2fbfc41d3bc9f36bbb48abf0bb179114e83c926
Colin Huber <cbhuber@gmail.com>
2020-12-10 21:02:23 -0700

    feat: Continue to build GNSS and signal endpoint APIs.

--- backend/src/gnssapi.py
+++ backend/src/gnssapi.py
@@ -3,6 +3,7 @@ import json
 from flask_cors import CORS
 
 from .database.models import setup_db, Gnss, Signal
+from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
@@ -39,6 +40,7 @@ def get_gnss():
 
 @app.route('/gnss-signals')
 # @requires_auth('TBD')
+# def get_gnss_signals(payload):
 def get_gnss_signals():
     '''Gets GNSS signals TBD.'''
 
@@ -60,57 +62,141 @@ def get_gnss_signals():
 
 # -----------------------------------------------------------------------------------------------------------
 
-# @app.route('/gnss', methods=['POST'])
+
+@app.route('/gnss', methods=['POST'])
 # @requires_auth('TBD')
 # def create_gnss(payload):
-#     '''Creates a new GNSS TBD.'''
+def create_gnss():
+    '''Creates a new GNSS TBD.'''
 
-#     return jsonify(result)
+    # TODO: Error handling
+
+    gnss_data = request.get_json()
+
+    print(gnss_data)
+
+    new_gnss = Gnss(**gnss_data)
+
+    # TODO: Add to db and error handling
+
+    print(new_gnss.format())
+
+    return jsonify({'success': True, 'gnss': [new_gnss.format()]})
 
 # -----------------------------------------------------------------------------------------------------------
 
-# @app.route('/gnss-signals', methods=['POST'])
+
+@app.route('/gnss-signals', methods=['POST'])
 # @requires_auth('TBD')
 # def create_gnss_signal(payload):
-#     '''Creates a new GNSS signal TBD.'''
+def create_gnss_signal():
+    '''Creates a new GNSS signal TBD.'''
 
-#     return jsonify(result)
+    # TODO: Error handling
+
+    gnss_signal_data = request.get_json()
+
+    print(gnss_signal_data)
+
+    new_gnss_signal = Signal(**gnss_signal_data)
+
+    # TODO: Add to db and error handling
+
+    print(new_gnss_signal.format())
+
+    return jsonify({'success': True, 'signal': [new_gnss_signal.format()]})
 
 # -----------------------------------------------------------------------------------------------------------
 
-# @app.route('/gnss/<int:gnss_id>', methods=['PATCH'])
+
+@app.route('/gnss/<int:gnss_id>', methods=['PATCH'])
 # @requires_auth('TBD')
 # def update_gnss(payload, gnss_id):
-#     '''Updates an existing GNSS TBD.'''
+def update_gnss(gnss_id):
+    '''Updates an existing GNSS TBD.'''
 
-#     return jsonify(result)
+    # TODO: Error handling
+
+    gnss_from_db = Gnss.query.get(gnss_id)
+
+    gnss_data = request.get_json()
+    print(gnss_data)
+
+    if 'name' in gnss_data:
+        gnss_from_db.name = gnss_data['name']
+
+    if 'owner' in gnss_data:
+        gnss_from_db.owner = gnss_data['owner']
+
+    if 'num_satellites' in gnss_data:
+        gnss_from_db.num_satellites = gnss_data['num_satellites']
+
+    if 'num_frequencies' in gnss_data:
+        gnss_from_db.num_frequencies = gnss_data['num_frequencies']
+
+    # TODO: Add to db and error handling
+
+    print(gnss_from_db.format())
+
+    return jsonify({'result': True, 'gnss': [gnss_from_db.format()]})
 
 # -----------------------------------------------------------------------------------------------------------
 
-# @app.route('/gnss-signals/<int:signal_id>', methods=['PATCH'])
+
+@app.route('/gnss-signals/<int:signal_id>', methods=['PATCH'])
 # @requires_auth('TBD')
 # def update_gnss_signal(payload, signal_id):
-#     '''Updates an existing GNSS signal TBD.'''
+def update_gnss_signal(signal_id):
+    '''Updates an existing GNSS signal TBD.'''
 
-#     return jsonify(result)
+    # TODO: Error handling
+
+    gnss_signal_from_db = Signal.query.get(signal_id)
+
+    signal_data = request.get_json()
+    print(signal_data)
+
+    if 'signal' in signal_data:
+        gnss_signal_from_db.signal = signal_data['signal']
+
+    if 'gnss_id' in signal_data:
+        gnss_signal_from_db.gnss_id = signal_data['gnss_id']
+
+    # TODO: Add to db and error handling
+
+    print(gnss_signal_from_db.format())
+
+    return jsonify({'result': True, 'signal': [gnss_signal_from_db.format()]})
 
 # -----------------------------------------------------------------------------------------------------------
 
-# @app.route('/gnss/<int:gnss_id>', methods=['DELETE'])
+
+@app.route('/gnss/<int:gnss_id>', methods=['DELETE'])
 # @requires_auth('TBD')
 # def delete_gnss(payload, gnss_id):
-#     '''Deletes an existing GNSS TBD.'''
+def delete_gnss(gnss_id):
+    '''Deletes an existing GNSS TBD.'''
 
-#     return jsonify(result)
+    gnss_to_delete = Gnss.query.get(gnss_id)
+
+    # TODO: Implement and error check
+
+    return jsonify({'success': True, 'delete': gnss_id})
 
 # -----------------------------------------------------------------------------------------------------------
 
-# @app.route('/gnss-signals/<int:signal_id>', methods=['DELETE'])
+
+@app.route('/gnss-signals/<int:signal_id>', methods=['DELETE'])
 # @requires_auth('TBD')
 # def delete_gnss_signal(payload, signal_id):
-#     '''Deletes an existing GNSS signal TBD.'''
+def delete_gnss_signal(signal_id):
+    '''Deletes an existing GNSS signal TBD.'''
 
-#     return jsonify(result)
+    gnss_signal_to_delete = Signal.query.get(signal_id)
+
+    # TODO: Implement and error check
+
+    return jsonify({'success': True, 'delete': signal_id})
 
 # -----------------------------------------------------------------------------------------------------------
 
@@ -143,9 +229,9 @@ def unprocessable(error):
 def internal_server_error(error):
     return jsonify({'success': False, 'error': 500, 'message': 'Internal server error'}), 500
 
-# TODO: Implement AuthError
-# @app.errorhandler(AuthError)
-# def auth_error(error):
-#     return jsonify({'success': False, 'error': error.status_code, 'message': error.error['description']}), error.status_code
+
+@app.errorhandler(AuthError)
+def auth_error(error):
+    return jsonify({'success': False, 'error': error.status_code, 'message': error.error['description']}), error.status_code
 
 # -----------------------------------------------------------------------------------------------------------
