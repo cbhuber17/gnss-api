4b27782420d6e8a68bd5bd6e1b74d2ef979e76e1
Colin Huber <cbhuber@gmail.com>
2020-12-13 14:36:47 -0700

    feat: Adding migrations to manage.py.

--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 from backend.src.gnssapi import create_app
+from backend.src.database.models import db
+
 
 app = create_app(test_config=None)
+migrate = Migrate(app, db)
+
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
